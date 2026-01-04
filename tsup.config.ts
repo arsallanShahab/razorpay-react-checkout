a29edbf0ebc91fa073b5e75055b57c2b9c4ed341
Arsallan Shahab <mdarsallan@gmail.com>
2026-01-04 18:05:58 +0530

    fix: add "use client" directive to RazorpayProvider and update tsup config for client-side support

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,27 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-    entry: ["src/index.ts", "src/server.ts"],
-    format: ["cjs", "esm"],
-    dts: true,
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    treeshake: true,
-    external: ["react", "react-dom"],
-});
+export default defineConfig([
+    {
+        entry: ["src/index.ts"],
+        format: ["cjs", "esm"],
+        dts: true,
+        splitting: false,
+        sourcemap: true,
+        clean: true,
+        treeshake: true,
+        external: ["react", "react-dom"],
+        banner: {
+            js: '"use client";',
+        },
+    },
+    {
+        entry: ["src/server.ts"],
+        format: ["cjs", "esm"],
+        dts: true,
+        splitting: false,
+        sourcemap: true,
+        clean: false,
+        treeshake: true,
+        external: ["react", "react-dom"],
+    },
+]);
